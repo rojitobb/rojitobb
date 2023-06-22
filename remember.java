51bb077e60a271c5b721368463dba8898ad843f0
rojitobb <130780268+rojitobb@users.noreply.github.com>
2023-06-22 08:30:52 -0500

    Update and rename remember java to remember.java

---
+++ remember.java
@@ -0,0 +1,10 @@
+//firts hello world
+
+class helloword{
+  public static void main(String agrs[]){
+
+ //UPPER_CASE : constante
+System.out.println(“Hello”);
+System.out.print(“World!!!”);
+System.out.printf(“format text”);
+  }}
